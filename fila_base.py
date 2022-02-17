360e7387b9a9ea2a14a54de28683d0f710bdf356
Zhokun <darlanjogos@gmail.com>
2022-02-17 17:18:00 -0300

    Criação do fila_base

---
+++ fila_base.py
@@ -0,0 +1,13 @@
+
+
+class FilaBase:
+    codigo = 0
+    fila: list = []
+    clientes_atendidos: list = []
+    senha_atual: str = ""
+
+    def reseta_fila(self) -> None:
+        if self.codigo >= 100:
+            self.codigo = 0
+        else:
+            self.codigo += 1
